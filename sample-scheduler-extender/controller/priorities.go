ff4057c13e01b3388ca884872e42ebadebe05619
the2bhm <32093506+the2bhm@users.noreply.github.com>
2020-06-12 13:43:23 +0800

    Update priorities.go

--- sample-scheduler-extender/controller/priorities.go
+++ sample-scheduler-extender/controller/priorities.go
@@ -11,19 +11,17 @@ const (
 )
 
 
-func prioritize(args schedulerapi.ExtenderArgs) *schedulerapi.HostPriorityList {
-	pod := args.Pod
-	nodes := args.Nodes.Items
-
-	hostPriorityList := make(schedulerapi.HostPriorityList, len(nodes))
-	for i, node := range nodes {
-		score := i+1
-		log.Printf(PrioMsg, pod.Name, pod.Namespace, score)
-		hostPriorityList[i] = schedulerapi.HostPriority{
-			Host:  node.Name,
-			Score: score,
-		}
-	}
-
-	return &hostPriorityList
-}
+func prioritize ( args schedulerapi.ExtenderArgs) *schedulerapi.HostPriorityList {
+  pod:=args.Pod
+  nodes:=args.Nodes.Items
+  hostPriorityList := make(schedulerapi.HostPriorityList, len(nodes))
+  for i, node := range nodes {
+      score := rand.Intn(schedulerapi.MaxPriority+1)// 在最大优先级内随机取一个值               
+      log.Printf(luckyPrioMsg,pod.Name,pod.Namespace,score)
+      hostPriorityList[i] = schedulerapi.HostPriority{ 
+          Host: node.Name,
+	  Score: score,
+	  }
+      }
+      return &hostPriorityList
+ }
